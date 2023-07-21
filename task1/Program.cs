e6abda0fbe79ceb37139800213caf60755e1c004
axlerya <white_cat_02@mail.ru>
2023-07-21 21:06:03 +0500

    Added task1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne == numberTwo)
+{
+    Console.Write("Числа равны");
+}
+else if (numberOne > numberTwo)
+{
+    Console.Write("Первое число" + " " + numberOne + " " + "больше чем втрое" +  " " + numberTwo);
+}
+else 
+{
+    Console.Write("Втрое чило" + " " + numberTwo + " " + "больше чем первое" + " "+ numberOne);
+}
