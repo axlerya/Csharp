d1743a68d1f3831fd0c09034d7bd9254678bc4fe
axlerya <white_cat_02@mail.ru>
2023-07-21 23:10:19 +0500

    Added task 4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+string numbersEven = "";
+int count =  number / 2; 
+int i = 0;
+int numberAdd = 0;
+
+while (i<count)
+{
+    i++;
+    numberAdd += 2;
+    numbersEven = numbersEven.Insert(numbersEven.Length, $" {numberAdd}");
+}
+Console.WriteLine($"Все четные числа до числа {number} - {numbersEven}");
