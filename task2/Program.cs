2d1dcfcc2c023d89d848deb178d9a219eb010424
axlerya <white_cat_02@mail.ru>
2023-07-21 21:44:55 +0500

    added taks2

---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (numberOne == numberTwo & numberTwo == numberThree & numberOne == numberThree)
+{
+    Console.WriteLine("Числа равны");
+}
+
+else if (numberOne > numberTwo & numberOne > numberThree)
+{
+    Console.WriteLine($"max = {numberOne} - первое число");
+}
+
+else if (numberTwo > numberOne & numberTwo > numberThree)
+{
+    Console.WriteLine($"max = {numberTwo} - второе число");
+}
+
+else if (numberThree > numberOne & numberThree > numberTwo)
+{
+    Console.WriteLine($"max = {numberThree} - третье число");
+}
+
+
