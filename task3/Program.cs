a160edd1fb574b8be32b928cbeb1e3fd19ffa95a
axlerya <white_cat_02@mail.ru>
2023-07-21 21:56:29 +0500

    Added task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberRemains = number % 2;
+
+if (numberRemains == 0)
+{ 
+    Console.WriteLine($"Число {number} - четное");
+}
+else
+{ 
+    Console.WriteLine($"Число {number} - нечетное");
+}
